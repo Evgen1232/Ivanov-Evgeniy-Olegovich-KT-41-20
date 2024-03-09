8bb5228713ad041be9a35fb76bbd2db61c24564b
Srrw <srrwssl@mail.ru>
2024-03-09 22:30:12 +0300

    feat: add dbcontet and CreateDatabase migration

---
+++ IvanovEvgeniyKT_41_20/Database/IvanovDbContext.cs
@@ -0,0 +1,24 @@
+﻿using IvanovEvgeniyKT_41_20.Database.Configurations;
+using IvanovEvgeniyKT_41_20.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IvanovEvgeniyKT_41_20.Database
+{
+    public class IvanovDbContext : DbContext
+    {
+        DbSet<Student> Students { get; set; }
+        DbSet<Group> Groups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Конфигурации к таблицам
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new GroupConfiguration());
+
+        }
+
+        public IvanovDbContext(DbContextOptions<IvanovDbContext> options) : base(options)
+        {
+        }
+    }
+}
